31f6711e2aec2477512ac7af4adefe763985def2
Adrian Bartnik <adrian.bartnik@web.de>
2017-02-12 17:47:30 -0800

    Added reveal animation to color selection activity

--- app/src/main/java/de/adrianbartnik/lightpainting/ui/colorselection/ColorSelectionActivity.kt
+++ app/src/main/java/de/adrianbartnik/lightpainting/ui/colorselection/ColorSelectionActivity.kt
@@ -5,14 +5,19 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.PorterDuff
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.view.View
 import android.widget.FrameLayout
 import com.android.colorpicker.ColorPickerPalette
 import com.android.colorpicker.ColorPickerSwatch
 import com.pes.androidmaterialcolorpickerdialog.ColorPicker
 import de.adrianbartnik.lightpainting.R
+import de.adrianbartnik.lightpainting.layout.RevealLayout
+import de.adrianbartnik.lightpainting.ui.painting.PaintingActivity
+
 
 class ColorSelectionActivity : AppCompatActivity() {
 
@@ -32,6 +37,41 @@ class ColorSelectionActivity : AppCompatActivity() {
         colorPickerPalette.drawPalette(colors, colors[0])
 
         setupCustomColor()
+        setupFAB()
+    }
+
+    private fun setupFAB() {
+
+        val mFab = findViewById(R.id.fab) as FloatingActionButton
+        val mRevealLayout = findViewById(R.id.reveal_layout) as RevealLayout
+        val mRevealView = findViewById(R.id.reveal_view)
+
+        mFab.setOnClickListener {
+            mFab.setClickable(false) // Avoid naughty guys clicking FAB again and again...
+            val location = IntArray(2)
+            mFab.getLocationOnScreen(location)
+            location[0] += mFab.getWidth() / 2
+            location[1] += mFab.getHeight() / 2
+
+            val intent = PaintingActivity.GetStartIntent(this@ColorSelectionActivity, PaintingActivity.PaintShape.CircleShape)
+
+            mRevealView.setVisibility(View.VISIBLE)
+            mRevealLayout.setVisibility(View.VISIBLE)
+
+            mRevealLayout.show(location[0], location[1]) // Expand from center of FAB. Actually, it just plays reveal animation.
+
+            mFab.postDelayed({
+                startActivity(intent)
+                 // Without using R.anim.hold, the screen will flash because of transition of Activities.
+                overridePendingTransition(0, R.anim.hold)
+            }, 600) // 600 is default duration of reveal animation in RevealLayout
+
+            mFab.postDelayed({
+                mFab.setClickable(true)
+                mRevealLayout.setVisibility(View.INVISIBLE)
+                mRevealView.setVisibility(View.INVISIBLE)
+            }, 960) // Or some numbers larger than 600.
+        }
     }
 
     private fun setupCustomColor() {
