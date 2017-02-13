14be63959be4d10144b39cdeec16eef1ff49c043
Adrian Bartnik <adrian.bartnik@web.de>
2017-02-12 18:18:28 -0800

    Added click to turn black behavior

---
+++ app/src/main/java/de/adrianbartnik/lightpainting/ui/painting/shape/BaseShape.kt
@@ -0,0 +1,43 @@
+package de.adrianbartnik.lightpainting.ui.painting.shape
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.view.View
+
+
+@SuppressLint("ViewConstructor")
+open class BaseShape(context: Context, rgbColor: Triple<Int, Int, Int>) : View(context) {
+
+    val paint = Paint()
+    val black = Paint()
+    var turnBlack = false
+
+    init {
+        paint.setARGB(255, rgbColor.first, rgbColor.second, rgbColor.third)
+        paint.strokeWidth = 8f
+        paint.isAntiAlias = true
+        paint.strokeCap = Paint.Cap.BUTT
+        paint.style = Paint.Style.FILL
+
+        black.style = Paint.Style.FILL
+        black.isAntiAlias = true
+        black.strokeWidth = 8f
+        black.setARGB(255, 0, 0, 0)
+
+        setOnClickListener {
+            turnBlack = !turnBlack
+            invalidate()
+        }
+    }
+
+    fun checkForTurnBlack(canvas: Canvas) : Boolean {
+
+        if (turnBlack) {
+            canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), black)
+        }
+
+        return turnBlack
+    }
+}
--- app/src/main/java/de/adrianbartnik/lightpainting/ui/painting/shape/Fullscreen.kt
+++ app/src/main/java/de/adrianbartnik/lightpainting/ui/painting/shape/Fullscreen.kt
@@ -3,25 +3,17 @@ package de.adrianbartnik.lightpainting.ui.painting.shape
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.Paint
-import android.view.View
 
 
 @SuppressLint("ViewConstructor")
-class Fullscreen(context: Context, rgbColor: Triple<Int, Int, Int>) : View(context) {
-
-    val paint = Paint()
-
-    init {
-        paint.setARGB(255, rgbColor.first, rgbColor.second, rgbColor.third)
-        paint.strokeWidth = 8f
-        paint.isAntiAlias = true
-        paint.strokeCap = Paint.Cap.BUTT
-        paint.style = Paint.Style.FILL
-    }
+class Fullscreen(context: Context, rgbColor: Triple<Int, Int, Int>) : BaseShape(context, rgbColor) {
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+
+        if (checkForTurnBlack(canvas)) {
+            return
+        }
 
         canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paint)
     }
