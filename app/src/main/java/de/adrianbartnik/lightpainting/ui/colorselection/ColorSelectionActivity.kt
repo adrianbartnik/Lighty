3d621ceab00f59c0a9077888311702cabf9aadcb
Adrian Bartnik <adrian.bartnik@web.de>
2017-02-13 21:28:48 -0800

    Revelbackground now showing same color as selected color

--- app/src/main/java/de/adrianbartnik/lightpainting/ui/colorselection/ColorSelectionActivity.kt
+++ app/src/main/java/de/adrianbartnik/lightpainting/ui/colorselection/ColorSelectionActivity.kt
@@ -3,6 +3,8 @@ package de.adrianbartnik.lightpainting.ui.colorselection
 
 import android.content.Context
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.graphics.PorterDuff
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
@@ -22,27 +24,31 @@ import de.adrianbartnik.lightpainting.ui.painting.PaintingActivity
 class ColorSelectionActivity : AppCompatActivity() {
 
     lateinit var colorPickerPalette : ColorPickerPalette
+    lateinit var mFab : FloatingActionButton
+    var currentColor : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_color_selection)
 
         setupActionbar()
+        setupFAB()
+        setupColorpalette()
+        setupCustomColor()
+    }
 
+    private fun setupColorpalette() {
         colorPickerPalette = findViewById(R.id.picker) as ColorPickerPalette
 
         val colors = resources.getIntArray(R.array.color_palette)
 
         colorPickerPalette.init(5)
         colorPickerPalette.drawPalette(colors, colors[0])
-
-        setupCustomColor()
-        setupFAB()
     }
 
     private fun setupFAB() {
 
-        val mFab = findViewById(R.id.fab) as FloatingActionButton
+        mFab = findViewById(R.id.fab) as FloatingActionButton
         val mRevealLayout = findViewById(R.id.reveal_layout) as RevealLayout
         val mRevealView = findViewById(R.id.reveal_view)
 
@@ -53,9 +59,12 @@ class ColorSelectionActivity : AppCompatActivity() {
             location[0] += mFab.getWidth() / 2
             location[1] += mFab.getHeight() / 2
 
-            val intent = PaintingActivity.GetStartIntent(this@ColorSelectionActivity, PaintShape.FullscreenShape)
+            val col = colorPickerPalette.currentSelectedColor?.color ?: currentColor
+
+            val intent = PaintingActivity.GetStartIntent(this@ColorSelectionActivity, PaintShape.FullscreenShape, col)
 
             mRevealView.setVisibility(View.VISIBLE)
+            mRevealView.setBackgroundColor(col)
             mRevealLayout.setVisibility(View.VISIBLE)
 
             mRevealLayout.show(location[0], location[1]) // Expand from center of FAB. Actually, it just plays reveal animation.
@@ -75,18 +84,16 @@ class ColorSelectionActivity : AppCompatActivity() {
     }
 
     private fun setupCustomColor() {
-        val defaultColor = ContextCompat.getColor(applicationContext, R.color.blue_grey)
-        val view = ColorPickerSwatch(this@ColorSelectionActivity, defaultColor, false, null)
+        setCurrentColder(ContextCompat.getColor(applicationContext, R.color.blue_grey))
+        val view = ColorPickerSwatch(this@ColorSelectionActivity, currentColor, false, null)
 
         val framelayout = findViewById(R.id.color_selection_custom_color) as FrameLayout
         framelayout.addView(view)
 
         view.setOnClickListener {
-            val defaultRed = resources.getInteger(R.integer.default_red)
-            val defaulGreen = resources.getInteger(R.integer.default_green)
-            val defaultBlue = resources.getInteger(R.integer.default_blue)
+            val default = ContextCompat.getColor(this, R.color.default_color_colorpicker)
 
-            val cp = ColorPicker(this@ColorSelectionActivity, defaultRed, defaulGreen, defaultBlue)
+            val cp = ColorPicker(this@ColorSelectionActivity, Color.red(default), Color.green(default), Color.blue(default))
             cp.setCanceledOnTouchOutside(true)
             cp.setCancelable(true)
             cp.show()
@@ -95,9 +102,16 @@ class ColorSelectionActivity : AppCompatActivity() {
                 view.setColor(col)
                 view.setChecked(true)
                 colorPickerPalette.clearCurrentSelection()
+                setCurrentColder(col)
                 cp.dismiss()
             }
         }
+    }
+
+    private fun setCurrentColder(col: Int) {
+        currentColor = col
+        mFab.backgroundTintList = ColorStateList.valueOf(col)
+        mFab.rippleColor = col
     }
 
     private fun setupActionbar() {
--- app/src/main/java/de/adrianbartnik/lightpainting/ui/painting/PaintingActivity.kt
+++ app/src/main/java/de/adrianbartnik/lightpainting/ui/painting/PaintingActivity.kt
@@ -3,6 +3,7 @@ package de.adrianbartnik.lightpainting.ui.painting
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import de.adrianbartnik.lightpainting.ui.colorselection.ColorSelectionActivity
@@ -13,7 +14,9 @@ class PaintingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(Fullscreen(this, Triple(11, 244, 234)))
+        val col =  intent.extras.getInt(EXTRA_COLOR)
+
+        setContentView(Fullscreen(this, Triple(Color.red(col), Color.green(col), Color.blue(col))))
 
         val shape = intent.extras?.containsKey(EXTRA_SHAPE) ?: false
 
@@ -25,11 +28,13 @@ class PaintingActivity : AppCompatActivity() {
 
     companion object {
 
-        private val EXTRA_SHAPE = "shape"
+        private val EXTRA_SHAPE = "painting_activity_shape"
+        private val EXTRA_COLOR = "painting_activity_color"
 
-        fun GetStartIntent(context: Context, shape: ColorSelectionActivity.PaintShape): Intent {
+        fun GetStartIntent(context: Context, shape: ColorSelectionActivity.PaintShape, color: Int): Intent {
             val intent = Intent(context, PaintingActivity::class.java)
             intent.putExtra(EXTRA_SHAPE, shape)
+            intent.putExtra(EXTRA_COLOR, color)
             return intent
         }
     }
