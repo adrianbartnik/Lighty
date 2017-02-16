298ddf64c93129fe070c352a2e0a5071574389b9
Adrian Bartnik <adrian.bartnik@web.de>
2017-02-15 23:12:49 -0800

    Hide navigation bar in painting activity

--- app/src/main/java/de/adrianbartnik/lightpainting/ui/painting/PaintingActivity.kt
+++ app/src/main/java/de/adrianbartnik/lightpainting/ui/painting/PaintingActivity.kt
@@ -3,12 +3,16 @@ package de.adrianbartnik.lightpainting.ui.painting
 
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import de.adrianbartnik.lightpainting.ui.colorselection.ColorSelectionActivity
 import de.adrianbartnik.lightpainting.ui.painting.shape.Bar
 import de.adrianbartnik.lightpainting.ui.painting.shape.Circle
 import de.adrianbartnik.lightpainting.ui.painting.shape.Fullscreen
+
+
 
 class PaintingActivity : AppCompatActivity() {
 
@@ -26,6 +30,41 @@ class PaintingActivity : AppCompatActivity() {
         }
 
         setContentView(view)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        hideNavigationBar()
+
+        window.decorView.setOnSystemUiVisibilityChangeListener { visibility ->
+            if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN == 0) {
+                hideNavigationBar()
+            }
+        }
+    }
+
+    private fun hideNavigationBar() {
+
+        var flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            flags = flags or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        }
+
+        window.decorView.systemUiVisibility = flags
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+
+        if (hasFocus) {
+            hideNavigationBar()
+        }
     }
 
     companion object {
